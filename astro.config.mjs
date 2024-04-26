3bf1c7682cf28024c36bbd627153f52914ff2e2d
Daniel Rojas <l20j45_@gmail.com>
2024-04-26 13:33:30 -0600

    Se agrego para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://l20j45.github.io',
-  base: 'my-repo',
+  base: 'tesla-clon',
 });
